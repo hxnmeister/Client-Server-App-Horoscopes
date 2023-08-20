557ef79d3bfc539b7ffafd39f98222461975c89d
Valerii <valeriy.stronskiy@gmail.com>
2023-08-20 14:53:51 +0300

    Creating a database and console application for the project.

---
+++ Створення бази данних Horoscopes.sql
@@ -0,0 +1,21 @@
+﻿create table ZodiacSigns
+(
+	Id int not null primary key,
+	Name varchar(11) not null
+)
+
+create table Predictions
+(
+	Id int not null primary key,
+	Content varchar(1000) not null
+)
+
+create table ZodiacsPredictions
+(
+	Id int not null primary key,
+	ZodiacSignId int not null,
+	PredictionId int not null
+
+	foreign key (ZodiacSignId) references ZodiacSigns(Id),
+	foreign key (PredictionId) references Predictions(Id)
+)
